8a8951271fa3eb376230825336a93560fe48064a
carlos4565mana <carloscal61@gmail.com>
2022-02-27 17:27:01 -0300

    Created class Curso, mentoria

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import br.dio.desafio.dominio.Curso;
+import br.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args){
+
+        Mentoria ment = new Mentoria();
+        ment.setTitulo("Java");
+        ment.setDescricao("Muito bom");
+        ment.setData(LocalDate.now());
+
+
+
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Java");
+        curso1.setTitulo("Pessimo");
+        curso1.setCargaHoraria(8);
+        System.out.println(curso1);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("C++");
+        curso2.setTitulo("Bom");
+        curso2.setCargaHoraria(10);
+        System.out.println(curso2);
+        System.out.println(ment);
+    }
+}
---
+++ src/br/dio/desafio/dominio/Mentoria.java
@@ -0,0 +1,34 @@
+package br.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+
+    private String titulo;
+    private String descricao;
+    private LocalDate data;
+
+    public Mentoria(){
+
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
