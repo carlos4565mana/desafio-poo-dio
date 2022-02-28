84ff43b999a116b32c0f8d4ee5cdbab833044b63
carlos4565mana <carloscal61@gmail.com>
2022-02-28 00:22:27 -0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,60 @@
-import br.dio.desafio.dominio.Conteudo;
-import br.dio.desafio.dominio.Curso;
-import br.dio.desafio.dominio.Mentoria;
+import br.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args){
 
-        Mentoria ment = new Mentoria();
-        ment.setTitulo("Java");
-        ment.setDescricao("Muito bom");
-        ment.setData(LocalDate.now());
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Java");
+        mentoria.setDescricao("Muito bom");
+        mentoria.setData(LocalDate.now());
 
         Curso curso1 = new Curso();
         curso1.setTitulo("Java");
-        curso1.setTitulo("Pessimo");
+        curso1.setDescricao("Bom");
         curso1.setCargaHoraria(8);
-        System.out.println(curso1);
+
 
         Curso curso2 = new Curso();
         curso2.setTitulo("C++");
-        curso2.setTitulo("Bom");
+        curso2.setDescricao("Bom");
         curso2.setCargaHoraria(10);
-        System.out.println(curso2);
-        System.out.println(ment);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Java Developer");
+        bootcamp.setDescricao("descricao Bootcamp Java developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devCarlos = new Dev();
+        devCarlos.setNome("carlos");
+        devCarlos.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos Carlos"+devCarlos.getConteudosInscritos());
+        devCarlos.progredir();
+        devCarlos.progredir();
+        System.out.println("--");
+        System.out.println("Conteudos inscritos Carlos"+devCarlos.getConteudosInscritos());
+        System.out.println("Conteudos concluidos Carlos"+devCarlos.getConteudosConcluidos());
+        System.out.println("XP:"+devCarlos.calcularTotalXP());
+
+
+
+        System.out.println("-------------");
+        Dev devJoao = new Dev();
+        devJoao.setNome("Joao");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos Joao"+devJoao.getConteudosInscritos());
+        devJoao.progredir();
+        devJoao.progredir();
+        devJoao.progredir();
+
+        System.out.println("--");
+        System.out.println("Conteudos inscritos Joao"+devJoao.getConteudosInscritos());
+        System.out.println("Conteudos concluidos Joao"+devCarlos.getConteudosConcluidos());
+        System.out.println("XP:"+devJoao.calcularTotalXP());
+
+
     }
 }
--- src/br/dio/desafio/dominio/Dev.java
+++ src/br/dio/desafio/dominio/Dev.java
@@ -8,7 +8,7 @@ import java.util.Set;
 public class Dev {
     private String nome;
     private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
-    private Set<Conteudo> conteudosComcluidos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
     public void inscreverBootcamp(Bootcamp bootcamp){
         this.conteudosInscritos.addAll(bootcamp.getConteudos());
@@ -18,7 +18,7 @@ public class Dev {
     public void progredir(){
         Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
         if(conteudo.isPresent()){
-            this.conteudosComcluidos.add(conteudo.get());
+            this.conteudosConcluidos.add(conteudo.get());
             this.conteudosInscritos.remove(conteudo.get());
         }else{
             System.err.println("Voce não esta matriculado em nenhum conteudo");
@@ -27,7 +27,7 @@ public class Dev {
 
     }
     public double calcularTotalXP(){
-       return this.conteudosComcluidos
+       return this.conteudosConcluidos
                .stream()
                .mapToDouble(conteudo -> conteudo.calcularXP())
                .sum();
@@ -50,12 +50,12 @@ public class Dev {
         this.conteudosInscritos = conteudosInscritos;
     }
 
-    public Set<Conteudo> getConteudosComcluidos() {
-        return conteudosComcluidos;
+    public Set<Conteudo> getConteudosConcluidos() {
+        return conteudosConcluidos;
     }
 
-    public void setConteudosComcluidos(Set<Conteudo> conteudosComcluidos) {
-        this.conteudosComcluidos = conteudosComcluidos;
+    public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
+        this.conteudosConcluidos = conteudosConcluidos;
     }
 
     @Override
@@ -63,11 +63,11 @@ public class Dev {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Dev dev = (Dev) o;
-        return Objects.equals(nome, dev.nome) && Objects.equals(conteudosInscritos, dev.conteudosInscritos) && Objects.equals(conteudosComcluidos, dev.conteudosComcluidos);
+        return Objects.equals(nome, dev.nome) && Objects.equals(conteudosInscritos, dev.conteudosInscritos) && Objects.equals(conteudosConcluidos, dev.conteudosConcluidos);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(nome, conteudosInscritos, conteudosComcluidos);
+        return Objects.hash(nome, conteudosInscritos, conteudosConcluidos);
     }
 }
